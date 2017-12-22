95dadd8ed18ecc4aed4879b5c77368bbc005b7cd
zhangjianfei <zhangjianfei@b.360.cn>
2017-12-22 11:18:07 +0800

    add demo

---
+++ 360_demo.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "github.com/golang/glog"
+)
+
+
+func main() {
+	// myLogging := glog.loggingT{
+	// 	toStderr: false,
+	// }
+	//myLogging.toStderr = false
+
+	// defer glog.logging.swap(glog.logging.newBuffers())
+	glog.Info("Prepare to repel boarders")
+}
